76f1e517d60f51debad56f57fa145f5a6660dcd7
Qellexi <99039851+Qellexi@users.noreply.github.com>
2025-04-22 23:16:39 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
+import os
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, file_name: str):
+        self.file_name = file_name
+        self.file = None
+
+    def __enter__(self):
+        self.file = open(self.file_name)
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.file.close()
+        os.remove(self.file_name)
