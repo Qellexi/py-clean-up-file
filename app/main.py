b30cc939f1a17116ba277e6b7a26894d812d0a69
Qellexi <99039851+Qellexi@users.noreply.github.com>
2025-04-22 23:19:21 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 import os
+from typing import TextIO
+
+
 class CleanUpFile:
-    def __init__(self, file_name: str):
+    def __init__(self, file_name: str) -> None:
         self.file_name = file_name
         self.file = None
 
-    def __enter__(self):
+    def __enter__(self) -> TextIO:
         self.file = open(self.file_name)
         return self.file
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
         self.file.close()
         os.remove(self.file_name)
