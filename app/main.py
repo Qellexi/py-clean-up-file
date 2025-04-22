62c647aba399987b39aeefdf8b4b759b3b03b3a9
Qellexi <99039851+Qellexi@users.noreply.github.com>
2025-04-22 23:57:40 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,40 +1,40 @@
-from __future__ import annotations
-
 import os
-from typing import Optional, Type, TextIO
+from typing import Optional, Type
 
 
 class CleanUpFile:
-    def __init__(self, file_name: str) -> None:
-        self.file_name = file_name
+    def __init__(self, filename: str) -> None:
+        self.filename = filename 
         self.file = None
 
-    def __enter__(self) -> TextIO:
+    def __enter__(self) -> "CleanUpFile":
         try:
-            self.file = open(self.file_name, "r")
+            if not os.path.exists(self.filename):
+                open(self.filename, "w").close()
+
+            self.file = open(self.filename, "r+")
         except FileNotFoundError:
-            print(f"Error: File '{self.file_name}' does not exist.")
+            print(f"Error: File '{self.filename}' does not exist.")
             raise
         except PermissionError:
-            print(f"Error: Permission denied for file '{self.file_name}'.")
+            print(f"Error: Permission denied for file '{self.filename}'.")
             raise
         except IOError as e:
             print(f"An I/O error occurred: {e}")
             raise
-        return self.file
+        return self
 
     def __exit__(self, exc_type: Optional[Type[BaseException]],
                  exc_val: Optional[BaseException],
                  exc_tb: Optional[object]) -> None:
         try:
             if self.file:
-                self.file.close()  # Закриваємо файл
-            os.remove(self.file_name)  # Видаляємо файл
+                self.file.close()
+            if os.path.exists(self.filename):
+                os.remove(self.filename)
         except FileNotFoundError:
-            print(f"Error: File '{self.file_name}' was not "
-                  f"found during deletion.")
+            print(f"Error: File '{self.filename}' was not found during deletion.")
         except PermissionError:
-            print(f"Error: Permission denied when trying "
-                  f"to delete '{self.file_name}'.")
+            print(f"Error: Permission denied when trying to delete '{self.filename}'.")
         except IOError as e:
             print(f"An I/O error occurred while deleting the file: {e}")
